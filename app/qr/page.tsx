666002b2e069bd9f3347dba1631cb7f3a0ccb61b
Devin Harris <hello@devinhrrs.me>
2025-01-19 10:39:10 -0500

    remove old QR code library for qrcode.react

--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -1,4 +1,4 @@
-import QRCode from 'react-qr-code';
+import { QRCodeSVG } from 'qrcode.react'
 import styles from './QRPage.module.scss'
 
 export default function Page() {
@@ -9,7 +9,14 @@ export default function Page() {
                 <h1>Your Last Fitness Journey Starts Now.</h1>
                 <p>Take the first step towards achieving your fitness goals with <strong>SINC</strong>. Whether you're building strength, cutting fat, or enhancing performance, our platform delivers tailored workouts, nutrition plans, and progress tracking all in one place. Scan the QR code to join now and take control of your transformation.</p>
                 <div className={styles.qrCode}>
-                    <QRCode  style={{ width: '100%', height: '100%' }} value='https://sincfit.com' bgColor='#09090b' fgColor='#FFFFFF'/>
+                    <QRCodeSVG 
+                        style={{ width: '100%', height: '100%' }} 
+                        value='https://sincfit.com' 
+                        bgColor='#09090b' 
+                        fgColor='#FFFFFF'
+                        title='Build your new physique.'
+                        level='M'
+                    />
                 </div>
             
             </div>
