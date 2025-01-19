a9d8450bd6fa97add328b5d2af20a64ca967e7f6
Devin Harris <hello@devinhrrs.me>
2025-01-19 10:29:19 -0500

    add QR code for quick signups

--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
                 <h1>Your Last Fitness Journey Starts Now.</h1>
                 <p>Take the first step towards achieving your fitness goals with <strong>SINC</strong>. Whether you're building strength, cutting fat, or enhancing performance, our platform delivers tailored workouts, nutrition plans, and progress tracking all in one place. Scan the QR code to join now and take control of your transformation.</p>
                 <div className={styles.qrCode}>
-                    <QRCode  style={{ width: '100%' }} value='https://sincfit.com' bgColor='#09090b' fgColor='#FFFFFF'/>
+                    <QRCode  style={{ width: '100%', height: '100%' }} value='https://sincfit.com' bgColor='#09090b' fgColor='#FFFFFF'/>
                 </div>
             
             </div>
